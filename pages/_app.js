3c97ed536a846789a1f1b615aac3efc3ce1bd034
Anto Carrizo Berroeta <antocarrizoberroeta@MacBook-Pro-de-Anto.local>
2023-09-28 23:23:36 -0300

    da

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,5 @@
 import '@/styles/globals.css'
-import { PreguntaProvider } from 'pages/PreguntaContext.js'; // Asegúrate de que la capitalización coincida
 
 export default function App({ Component, pageProps }) {
-  return (
-    <PreguntaProvider>
-      <Component {...pageProps} />
-    </PreguntaProvider>
-  );
+  return <Component {...pageProps} />
 }
